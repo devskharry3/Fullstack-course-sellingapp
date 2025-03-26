72e8594fb572faed89abd8c5effe17cb909b2b83
osho sunkanmi harry <oshosunkanmi270@gmail.com>
2025-03-26 10:57:40 +0100

    updated cors

--- Backend/index.js
+++ Backend/index.js
@@ -21,12 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors  ({
-  origin: process.env.FRONTEND_URL,
-  credentials:true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}))
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+}));
 
 // ✅ Fix Cloudinary Config - Ensure Correct ENV Names
 cloudinary.config({
